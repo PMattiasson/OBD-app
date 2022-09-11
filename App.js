99f36ba65f9ac7e9e9bb88555977aed07342cf18
PMattiasson <pmattiasson99@gmail.com>
2022-09-11 22:05:51 +0200

    Fixed issue with Android

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { useState } from 'react';
-import { Button } from 'react-native-web';
 
 export default function App() {
   const [var1, setVar1] = useState(0); 
   return (
     <View style={styles.container}>
       <Text>Cookies: {var1}</Text>
-      <Button onPress={()=>{setVar1(var1+1)}} title="Bake a Cookie!"></Button>
+      <Button onPress={()=>{setVar1(var1+1)}} title="Bake a Cookie!"/>
       <StatusBar style="auto" />
     </View>
   );
