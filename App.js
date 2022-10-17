36a96558aff08fe84d096c4c6d3564f58923e2c9
PMattiasson <pmattiasson99@gmail.com>
2022-10-18 00:39:30 +0200

    Light theme

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
-import OBD from './Components/OBD';
+import { MD3LightTheme as DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+// import OBD from './Components/OBD';
+import Bluetooth from './Components/Bluetooth';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  version: 3,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    secondary: '#f1c40f',
+    tertiary: '#a1b2c3'
+  },
+};
 
 export default function App() {
   return (
-    <PaperProvider>
-      <OBD/>
+    <PaperProvider theme={theme}>
+      <Bluetooth/>
     </PaperProvider>
   );
 }
