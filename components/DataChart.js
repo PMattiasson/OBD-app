925363140b780149fe4ffa369dc2b421f6a58d1d
PMattiasson <pmattiasson99@gmail.com>
2022-11-05 17:22:02 +0100

    Limited data shown in line chart

--- components/DataChart.js
+++ components/DataChart.js
@@ -4,7 +4,10 @@ import { Text } from 'react-native-paper';
 import { LineChart } from 'react-native-chart-kit';
 
 export default function DataChart(props) {
-  const chartData = props.data.arr;
+  const data = props.data.arr;
+  if (data.length > 20) {
+    chartData = data.slice(-20);
+  }
   const windowWidth = Dimensions.get("window").width;
     if (chartData.length > 0) {
         return (
