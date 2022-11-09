0980586a0322706594133090382be7689b948c61
PMattiasson <pmattiasson99@gmail.com>
2022-11-09 03:23:54 +0100

    Miscellaneous

--- app.config.js
+++ app.config.js
@@ -1,16 +1,16 @@
 const IS_DEV = process.env.APP_VARIANT === 'development';
 
 module.exports = ({ config }) => {
-  // You can also switch out the app icon and other properties to further
-  // differentiate the app on your device.
-  return {
-    ...config,
-    name: IS_DEV ? 'OBD (Development)' : 'OBD',
-    ios: {
-        bundleIdentifier: IS_DEV ? 'com.per.obd.dev' : 'com.per.obd',
-    },
-    android: {
-        package: IS_DEV ? 'com.per.obd.dev' : 'com.per.obd',
-    },
-    }
+    // You can also switch out the app icon and other properties to further
+    // differentiate the app on your device.
+    return {
+        ...config,
+        name: IS_DEV ? 'OBD (Dev)' : 'OBD',
+        ios: {
+            bundleIdentifier: IS_DEV ? 'com.per.obd.dev' : 'com.per.obd',
+        },
+        android: {
+            package: IS_DEV ? 'com.per.obd.dev' : 'com.per.obd',
+        },
+    };
 };
