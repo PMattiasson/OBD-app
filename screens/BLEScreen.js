e96acd5f24dcb2d05907d41e33e84fd4e91b124d
PMattiasson <pmattiasson99@gmail.com>
2022-11-19 12:57:45 +0100

    Added snackbar & made it more pretty

--- screens/BLEScreen.js
+++ screens/BLEScreen.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { View, LogBox } from 'react-native';
-import { Text, Button, Switch } from 'react-native-paper';
+import { Text, Button, Switch, Card, Snackbar } from 'react-native-paper';
 import { styles } from '../theme/styles';
+import { theme } from '../theme/theme';
 import DropDownPicker from 'react-native-dropdown-picker';
 import useBLE from '../components/useBLE';
 
@@ -21,13 +22,20 @@ export default function BLEScreen() {
     // Dropdown menu
     const [showDropDown, setShowDropDown] = useState(false);
     const [items, setItems] = useState([
-        { label: '0D : Vehicle speed', value: '02 01 0D' },
-        { label: '0C : Engine speed', value: '02 01 0C' },
+        { label: '0D - Vehicle speed', value: '02 01 0D' },
+        { label: '0C - Engine speed', value: '02 01 0C' },
     ]);
 
     // Connect button
     const [buttonIcon, setButtonIcon] = useState('bluetooth');
     const [buttonText, setButtonText] = useState('Connect');
+    const [buttonColor, setButtonColor] = useState(theme.colors.primary);
+
+    // Snackbar
+    const [visible, setVisible] = useState(false);
+    const [snackbarText, setSnackbarText] = useState('');
+    const onToggleSnackBar = () => setVisible(!visible);
+    const onDismissSnackBar = () => setVisible(false);
 
     function ConnectButton({ style }) {
         return (
@@ -46,9 +54,13 @@ export default function BLEScreen() {
 
     function handlePress() {
         if (bleState.isConnected) {
+            setSnackbarText(`Disconnected from ${bleState.connectedDevice?.name}`);
+            onToggleSnackBar();
             disconnectFromDevice();
         } else if (bleState.isLoading) {
             stopScan();
+            setSnackbarText('Stopped scanning for devices');
+            onToggleSnackBar();
         } else {
             scanDevices();
         }
@@ -58,6 +70,9 @@ export default function BLEScreen() {
         if (bleState.isConnected) {
             setButtonText('Connected');
             setButtonIcon('bluetooth-connect');
+            setSnackbarText(`Successfully connected to ${bleState.connectedDevice?.name}`);
+            onToggleSnackBar();
+            setButtonColor('limegreen');
         } else if (bleState.isLoading) {
             setButtonText('Scanning');
             setButtonIcon('refresh');
@@ -66,10 +81,12 @@ export default function BLEScreen() {
             case 'PoweredOff':
                 setButtonText('Bluetooth disabled');
                 setButtonIcon('bluetooth-off');
+                setButtonColor('red');
                 break;
             case 'PoweredOn':
                 setButtonText('Connect');
                 setButtonIcon('bluetooth');
+                setButtonColor(theme.colors.primary);
                 break;
             default:
                 setButtonText(bleState.adapterState);
@@ -80,9 +97,22 @@ export default function BLEScreen() {
 
     return (
         <View style={styles.container.center}>
-            <Text style={styles.text.title}>BLEScreen</Text>
-            <ConnectButton style={styles.button.primary} />
+            <Snackbar
+                wrapperStyle={{ top: 0 }}
+                style={styles.container.snackbar}
+                visible={visible}
+                onDismiss={onDismissSnackBar}
+                duration={3000}
+            >
+                <Text style={{ textAlign: 'center' }}>{snackbarText}</Text>
+            </Snackbar>
+
+            <Text style={styles.text.title}>Connect to the OBD reader</Text>
+
+            <ConnectButton style={[styles.button.primary, { backgroundColor: buttonColor }]} />
+
             <DropDownPicker
+                containerStyle={{ marginVertical: 20 }}
                 searchable={true}
                 searchPlaceholder={'Search or add custom command'}
                 addCustomItem={true}
@@ -97,8 +127,16 @@ export default function BLEScreen() {
                     bleState.isConnected && writeToCharacteristic(item.value);
                 }}
             />
-            <Text style={styles.text.base}>Sent request: {bleState.request}</Text>
-            <Text style={styles.text.base}>Received response: {bleState.response}</Text>
+
+            <View style={styles.item.row}>
+                <Card style={styles.card.card}>
+                    <Card.Content>
+                        <Text style={styles.text.base}>Sent request: {bleState.request}</Text>
+                        <Text style={styles.text.base}>Received response: {bleState.response}</Text>
+                    </Card.Content>
+                </Card>
+            </View>
+
             <Text style={styles.text.base}>BLE status: {bleState.status}</Text>
         </View>
     );
