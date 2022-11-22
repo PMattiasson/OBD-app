7972c5d2c53d8365a693a5bddc4d0aee3a650b48
PMattiasson <pmattiasson99@gmail.com>
2022-11-22 09:54:39 +0100

    Added card and data text

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
-import { Text, Button } from 'react-native-paper';
-import { styles } from '../theme/styles';
+import { Text, Button, Switch, Card, Avatar } from 'react-native-paper';
+import { styles } from '../styles/styles';
 import Speedometer from '../components/Speedometer';
 import { useData } from '../components/DataContext';
 
 export default function DataScreen() {
     const data = useData();
-
     return (
         <View style={styles.container.base}>
+            <View style={{ alignItems: 'center' }}>
+                <Card.Title
+                    style={styles.card.home}
+                    title="On-Board Diagnostics"
+                    subtitle="Project in Embedded Systems"
+                    titleVariant="titleLarge"
+                    titleStyle={styles.titleText}
+                    subtitleStyle={styles.baseText}
+                    left={(props) => <Avatar.Icon {...props} icon="engine" />}
+                />
+            </View>
             <View style={styles.container.center}>
                 <View style={styles.item.row}>
-                    <Text style={styles.text.title}>DataScreen</Text>
+                    {data.value !== null && (
+                        <Text style={styles.text.title}>
+                            {data.description}: {data.value} {data.unit}
+                        </Text>
+                    )}
                 </View>
             </View>
             <Speedometer speedKPH={data?.value} />
