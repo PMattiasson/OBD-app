098e01be4eaa6a66a86b9945249acb028e19dc3f
PMattiasson <pmattiasson99@gmail.com>
2022-11-22 09:57:12 +0100

    feat: POST method to server API

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,6 +7,39 @@ import { useData } from '../components/DataContext';
 
 export default function DataScreen() {
     const data = useData();
+    const [togglePostData, setTogglePostData] = useState(false);
+    const onToggleSwitch = () => setTogglePostData(!togglePostData);
+
+    const apiURL = process.env.URL;
+
+    useEffect(() => {
+        const postData = async () => {
+            const response = await fetch(apiURL, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ data: `${data.description}: ${data.value} ${data.unit}` }),
+            }).catch((error) => console.log(error));
+            const text = await response.text();
+            return text;
+        };
+
+        // let interval;
+        // if (data.value && togglePostData) {
+        //     interval = setInterval(() => {
+        //         postData().then((res) => console.log('Server response: ', res));
+        //     }, 5000);
+        // }
+        if (togglePostData) {
+            postData();
+            // postData().then((res) => console.log('Server response: ', res));
+        }
+
+        // return () => clearInterval(interval);
+    }, [data.value, data.description, data.unit, togglePostData, apiURL]);
+
     return (
         <View style={styles.container.base}>
             <View style={{ alignItems: 'center' }}>
@@ -19,6 +52,10 @@ export default function DataScreen() {
                     subtitleStyle={styles.baseText}
                     left={(props) => <Avatar.Icon {...props} icon="engine" />}
                 />
+                <View style={styles.item.switch}>
+                    <Text>Toggle post API</Text>
+                    <Switch value={togglePostData} onValueChange={onToggleSwitch} />
+                </View>
             </View>
             <View style={styles.container.center}>
                 <View style={styles.item.row}>
