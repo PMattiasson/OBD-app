cb4ec2982ea6828fe077d218100519b31c1abf74
PMattiasson <pmattiasson99@gmail.com>
2022-12-08 03:03:08 +0100

    New settings screen

---
+++ screens/SettingsScreens.js
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+import { styles } from '../styles/styles';
+
+export default function SettingsScreen() {
+    return (
+        <View style={styles.container.center}>
+            <Text>Settings</Text>
+        </View>
+    );
+}
