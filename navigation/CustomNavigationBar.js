ab2cffbb8ad664adc56ab37ad0a455850700da72
PMattiasson <pmattiasson99@gmail.com>
2022-12-08 03:02:51 +0100

    Custom header bar

---
+++ navigation/CustomNavigationBar.js
@@ -0,0 +1,40 @@
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import { useState } from 'react';
+import { Appbar, Menu } from 'react-native-paper';
+
+export default function CustomNavigationBar({ route, navigation, back }) {
+    const [visible, setVisible] = useState(false);
+    const openMenu = () => setVisible(true);
+    const closeMenu = () => setVisible(false);
+
+    return (
+        <Appbar.Header mode="center-aligned" elevated={true} statusBarHeight={0}>
+            {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
+            <Appbar.Content title={getHeaderTitle(route)} />
+            <Menu
+                visible={visible}
+                onDismiss={closeMenu}
+                anchor={<Appbar.Action icon={'dots-vertical'} onPress={openMenu} />}
+            >
+                <Menu.Item
+                    onPress={() => {
+                        closeMenu();
+                        navigation.navigate('Settings');
+                    }}
+                    title="Settings"
+                />
+            </Menu>
+        </Appbar.Header>
+    );
+}
+
+function getHeaderTitle(route) {
+    const routeName = getFocusedRouteNameFromRoute(route) ?? route.name;
+
+    switch (routeName) {
+    case 'Root':
+        return 'Home';
+    default:
+        return routeName;
+    }
+}
