acf8a42745828ce95a0a496c7fe86c5341883762
PMattiasson <pmattiasson99@gmail.com>
2022-12-08 03:02:23 +0100

    Reorganized Stack and Tab Navigators

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,86 +1,60 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { getFocusedRouteNameFromRoute, NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { MaterialCommunityIcons as Icon } from '@expo/vector-icons';
 import HomeScreen from '../screens/HomeScreen';
 import BluetoothScreen from '../screens/BluetoothScreen';
 import DeviceScreen from '../screens/DeviceScreen';
+import SettingsScreen from '../screens/SettingsScreens';
 import { theme } from '../styles/theme';
-import { Appbar } from 'react-native-paper';
+import CustomNavigationBar from './CustomNavigationBar';
 
-const HomeStack = createStackNavigator();
-
-function HomeStackScreen() {
-    return (
-        <HomeStack.Navigator
-            screenOptions={{
-                header: (props) => <CustomNavigationBar {...props} />,
-            }}
-        >
-            <HomeStack.Screen name="Home" component={HomeScreen} />
-        </HomeStack.Navigator>
-    );
-}
-
-const BluetoothStack = createStackNavigator();
-
-function BlueToothStackScreen() {
-    return (
-        <BluetoothStack.Navigator
-            screenOptions={{
-                headerMode: 'screen',
-                header: (props) => <CustomNavigationBar {...props} />,
-            }}
-        >
-            <BluetoothStack.Screen name="Bluetooth" component={BluetoothScreen} />
-            <BluetoothStack.Screen name="Devices" component={DeviceScreen} />
-        </BluetoothStack.Navigator>
-    );
-}
-
-function CustomNavigationBar({ route, navigation, back }) {
-    const title = route.name;
-    return (
-        <Appbar.Header mode="center-aligned" elevated={true} statusBarHeight={0}>
-            {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
-            <Appbar.Content title={title} />
-        </Appbar.Header>
-    );
-}
+const NavigationBarOption = {
+    headerMode: 'screen',
+    header: (props) => <CustomNavigationBar {...props} />,
+};
 
 const Tab = createMaterialBottomTabNavigator();
 
 function TabStack() {
     return (
-        <NavigationContainer theme={theme}>
-            <Tab.Navigator>
-                <Tab.Screen
-                    name="Speedometer"
-                    component={HomeStackScreen}
-                    options={{
-                        tabBarLabel: 'Home',
-                        tabBarIcon: () => {
-                            return <Icon name="speedometer" size={24} />;
-                        },
-                        tabBarColor: theme.colors.tabs[0],
-                    }}
-                />
-                <Tab.Screen
-                    name="BLE"
-                    component={BlueToothStackScreen}
-                    options={{
-                        tabBarLabel: 'Connect',
-                        tabBarIcon: () => {
-                            return <Icon name="bluetooth" size={24} />;
-                        },
-                        tabBarColor: theme.colors.tabs[1],
-                    }}
-                />
-            </Tab.Navigator>
-        </NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: () => {
+                        return <Icon name="speedometer" size={24} />;
+                    },
+                    tabBarColor: theme.colors.tabs[0],
+                }}
+            />
+            <Tab.Screen
+                name="Bluetooth"
+                component={BluetoothScreen}
+                options={{
+                    tabBarLabel: 'Connect',
+                    tabBarIcon: () => {
+                        return <Icon name="bluetooth" size={24} />;
+                    },
+                    tabBarColor: theme.colors.tabs[1],
+                }}
+            />
+        </Tab.Navigator>
     );
 }
 
+const Stack = createStackNavigator();
+
 export default function StackNavigator() {
-    return <TabStack />;
+    return (
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator screenOptions={NavigationBarOption}>
+                <Stack.Screen name="Root" component={TabStack} />
+                <Stack.Screen name="Settings" component={SettingsScreen} />
+                <Stack.Screen name="Devices" component={DeviceScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
