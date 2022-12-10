6b8ae84224cc0dae336570f45269de614b63d2b8
PMattiasson <pmattiasson99@gmail.com>
2022-12-10 18:13:00 +0100

    New unfinished settings

--- screens/SettingsScreens.js
+++ screens/SettingsScreens.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { Checkbox, Divider, List, Modal, Portal, RadioButton, Text } from 'react-native-paper';
 import { styles } from '../styles/styles';
 
-export default function SettingsScreen() {
+export default function SettingsScreen({ navigation }) {
     const [isSwitchOn, setIsSwitchOn] = useState(false);
     const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
 
@@ -13,8 +13,8 @@ export default function SettingsScreen() {
     const hideModal = () => setVisible(false);
     const containerStyle = { backgroundColor: 'white', padding: 20, borderRadius: 5 };
 
-    return (
-        <View style={styles.container.base}>
+    function SettingsModal({ children }) {
+        return (
             <Portal>
                 <Modal
                     visible={visible}
@@ -37,8 +37,42 @@ export default function SettingsScreen() {
                     </RadioButton.Group>
                 </Modal>
             </Portal>
+        );
+    }
+
+    return (
+        <ScrollView style={styles.container.base}>
+            <SettingsModal />
+
             <List.Section>
+                <List.Subheader>Bluetooth</List.Subheader>
+                <List.Item
+                    title="Device"
+                    description={'Not defined'}
+                    left={(props) => <List.Icon {...props} icon="devices" />}
+                    onPress={() => navigation.navigate('Devices')}
+                />
+                <List.Item
+                    title="Auto-connect"
+                    left={(props) => <List.Icon {...props} icon="bluetooth-connect" />}
+                    right={(props) => <Checkbox status={isSwitchOn ? 'checked' : 'unchecked'} />}
+                    onPress={onToggleSwitch}
+                />
+                <List.Item
+                    title="Update frequency"
+                    description={value ?? 'Not defined'}
+                    left={(props) => <List.Icon {...props} icon="update" />}
+                    onPress={showModal}
+                />
+
+                <Divider />
+
                 <List.Subheader>Cloud Server</List.Subheader>
+                <List.Item
+                    title="API address"
+                    description={process.env.URL ?? 'Not defined'}
+                    left={(props) => <List.Icon {...props} icon="link" />}
+                />
                 <List.Item
                     title="Toggle upload"
                     left={(props) => <List.Icon {...props} icon="cloud-upload-outline" />}
@@ -46,14 +80,9 @@ export default function SettingsScreen() {
                     onPress={onToggleSwitch}
                 />
                 <List.Item
-                    title="API address"
-                    description={process.env.URL ?? 'Not defined'}
-                    left={(props) => <List.Icon {...props} icon="link" />}
-                />
-                <List.Item
                     title="Upload frequency"
                     description={value ?? 'Not defined'}
-                    left={(props) => <List.Icon {...props} icon="clock-outline" />}
+                    left={(props) => <List.Icon {...props} icon="update" />}
                     onPress={showModal}
                 />
                 <List.Item
@@ -61,9 +90,36 @@ export default function SettingsScreen() {
                     description={'Not defined'}
                     left={(props) => <List.Icon {...props} icon="package-variant-closed" />}
                 />
+
                 <Divider />
+
                 <List.Subheader>Speedometer</List.Subheader>
+                <List.Item
+                    title="Variant style"
+                    description={'Not defined'}
+                    left={(props) => <List.Icon {...props} icon="speedometer" />}
+                />
+
+                <Divider />
+
+                <List.Subheader>Maps</List.Subheader>
+                <List.Item
+                    title="Toggle GPS"
+                    left={(props) => <List.Icon {...props} icon="map-outline" />}
+                    right={(props) => <Checkbox status={isSwitchOn ? 'checked' : 'unchecked'} />}
+                    onPress={onToggleSwitch}
+                />
+
+                <Divider />
+
+                <List.Subheader>Theme</List.Subheader>
+                <List.Item
+                    title="Dark mode"
+                    left={(props) => <List.Icon {...props} icon="theme-light-dark" />}
+                    right={(props) => <Checkbox status={isSwitchOn ? 'checked' : 'unchecked'} />}
+                    onPress={onToggleSwitch}
+                />
             </List.Section>
-        </View>
+        </ScrollView>
     );
 }
