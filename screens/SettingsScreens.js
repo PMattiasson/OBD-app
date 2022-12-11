7b024369344b12568d7b828f74e5e3033b4faf17
PMattiasson <pmattiasson99@gmail.com>
2022-12-11 05:35:54 +0100

    Unfinished testing of settings context

--- screens/SettingsScreens.js
+++ screens/SettingsScreens.js
@@ -1,125 +1,202 @@
-import { useState } from 'react';
+/* eslint-disable react-native/no-raw-text */
+import { useState, useEffect } from 'react';
 import { ScrollView, View } from 'react-native';
 import { Checkbox, Divider, List, Modal, Portal, RadioButton, Text } from 'react-native-paper';
 import { styles } from '../styles/styles';
+import { useSettings, useSettingsDispatch } from '../context/SettingsContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function SettingsScreen({ navigation }) {
+    const settings = useSettings();
+    const dispatch = useSettingsDispatch();
     const [isSwitchOn, setIsSwitchOn] = useState(false);
     const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
 
-    const [value, setValue] = useState(1);
-    const [visible, setVisible] = useState(false);
-    const showModal = () => setVisible(true);
-    const hideModal = () => setVisible(false);
+    const [modal, setModal] = useState({ visible: false, type: null });
+    const showModal = () => setModal({ ...modal, visible: true });
+    const hideModal = () => setModal({ ...modal, visible: false });
     const containerStyle = { backgroundColor: 'white', padding: 20, borderRadius: 5 };
 
-    function SettingsModal({ children }) {
+    const [value, setValue] = useState(1);
+
+    function SettingsModal() {
+        const SelectModal = settingModals[modal.type];
         return (
             <Portal>
                 <Modal
-                    visible={visible}
+                    visible={modal.visible}
                     onDismiss={hideModal}
                     contentContainerStyle={containerStyle}
                     style={{ padding: 20 }}
                 >
-                    <Text>Upload frequency</Text>
-                    <RadioButton.Group
-                        onValueChange={(value) => {
-                            hideModal();
-                            setValue(value);
-                        }}
-                        value={value}
-                    >
-                        <RadioButton.Item label="1 Hz" value={1000} />
-                        <RadioButton.Item label="5 Hz" value={200} />
-                        <RadioButton.Item label="10 Hz" value={100} />
-                        <RadioButton.Item label="20 Hz" value={50} />
-                    </RadioButton.Group>
+                    {modal.type && <SelectModal />}
                 </Modal>
             </Portal>
         );
     }
 
+    const settingModals = {
+        UpdateFrequency: BluetoothUpdateFrequencyModal,
+        PacketSize: ModalContent2,
+    };
+
+    function BluetoothUpdateFrequencyModal() {
+        return (
+            <>
+                <Text>Upload frequency</Text>
+                <RadioButton.Group
+                    onValueChange={(value) => {
+                        hideModal();
+                        dispatch({
+                            type: 'SET',
+                            object: 'bluetooth',
+                            property: 'updateFrequency',
+                            value: value,
+                        });
+                    }}
+                    value={settings.bluetooth.updateFrequency}
+                >
+                    <RadioButton.Item label="1 Hz" value={1000} />
+                    <RadioButton.Item label="5 Hz" value={200} />
+                    <RadioButton.Item label="10 Hz" value={100} />
+                    <RadioButton.Item label="20 Hz" value={50} />
+                </RadioButton.Group>
+            </>
+        );
+    }
+
+    function ModalContent2() {
+        return (
+            <>
+                <Text>Packet size</Text>
+                <RadioButton.Group
+                    onValueChange={(value) => {
+                        hideModal();
+                        setValue(value);
+                    }}
+                    value={value}
+                >
+                    <RadioButton.Item label="1" value={1} />
+                    <RadioButton.Item label="2" value={2} />
+                    <RadioButton.Item label="5" value={5} />
+                    <RadioButton.Item label="10" value={10} />
+                </RadioButton.Group>
+            </>
+        );
+    }
+
     return (
-        <ScrollView style={styles.container.base}>
+        <>
             <SettingsModal />
+            <ScrollView style={styles.container.base}>
+                <List.Section>
+                    <List.Subheader>Bluetooth</List.Subheader>
+                    <List.Item
+                        title="Device"
+                        description={settings.bluetooth.deviceName ?? 'Not defined'}
+                        left={(props) => <List.Icon {...props} icon="devices" />}
+                        onPress={() => navigation.navigate('Devices')}
+                    />
+                    <List.Item
+                        title="Auto-connect"
+                        left={(props) => <List.Icon {...props} icon="bluetooth-connect" />}
+                        right={(props) => (
+                            <Checkbox
+                                status={settings.bluetooth.autoConnect ? 'checked' : 'unchecked'}
+                            />
+                        )}
+                        onPress={() =>
+                            dispatch({
+                                type: 'TOGGLE',
+                                object: 'bluetooth',
+                                property: 'autoConnect',
+                            })
+                        }
+                    />
+                    <List.Item
+                        title="Update frequency"
+                        description={settings.bluetooth.updateFrequency ?? 'Not defined'}
+                        left={(props) => <List.Icon {...props} icon="update" />}
+                        onPress={() => {
+                            setModal({ visible: true, type: 'UpdateFrequency' });
+                        }}
+                    />
 
-            <List.Section>
-                <List.Subheader>Bluetooth</List.Subheader>
-                <List.Item
-                    title="Device"
-                    description={'Not defined'}
-                    left={(props) => <List.Icon {...props} icon="devices" />}
-                    onPress={() => navigation.navigate('Devices')}
-                />
-                <List.Item
-                    title="Auto-connect"
-                    left={(props) => <List.Icon {...props} icon="bluetooth-connect" />}
-                    right={(props) => <Checkbox status={isSwitchOn ? 'checked' : 'unchecked'} />}
-                    onPress={onToggleSwitch}
-                />
-                <List.Item
-                    title="Update frequency"
-                    description={value ?? 'Not defined'}
-                    left={(props) => <List.Icon {...props} icon="update" />}
-                    onPress={showModal}
-                />
+                    <Divider />
 
-                <Divider />
+                    <List.Subheader>Cloud Server</List.Subheader>
+                    <List.Item
+                        title="API address"
+                        description={settings.server.apiURL ?? 'Not defined'}
+                        left={(props) => <List.Icon {...props} icon="link" />}
+                    />
+                    <List.Item
+                        title="Toggle upload"
+                        left={(props) => <List.Icon {...props} icon="cloud-upload-outline" />}
+                        right={(props) => (
+                            <Checkbox
+                                status={settings.server.toggleUpload ? 'checked' : 'unchecked'}
+                            />
+                        )}
+                        onPress={() =>
+                            dispatch({
+                                type: 'TOGGLE',
+                                object: 'server',
+                                property: 'toggleUpload',
+                            })
+                        }
+                    />
+                    <List.Item
+                        title="Upload frequency"
+                        description={value ?? 'Not defined'}
+                        left={(props) => <List.Icon {...props} icon="update" />}
+                        onPress={() => {
+                            setModal({ visible: true, type: 'UploadFrequency' });
+                        }}
+                    />
+                    <List.Item
+                        title="Packet size"
+                        description={'Not defined'}
+                        left={(props) => <List.Icon {...props} icon="package-variant-closed" />}
+                        onPress={() => {
+                            setModal({ visible: true, type: 'PacketSize' });
+                        }}
+                    />
 
-                <List.Subheader>Cloud Server</List.Subheader>
-                <List.Item
-                    title="API address"
-                    description={process.env.URL ?? 'Not defined'}
-                    left={(props) => <List.Icon {...props} icon="link" />}
-                />
-                <List.Item
-                    title="Toggle upload"
-                    left={(props) => <List.Icon {...props} icon="cloud-upload-outline" />}
-                    right={(props) => <Checkbox status={isSwitchOn ? 'checked' : 'unchecked'} />}
-                    onPress={onToggleSwitch}
-                />
-                <List.Item
-                    title="Upload frequency"
-                    description={value ?? 'Not defined'}
-                    left={(props) => <List.Icon {...props} icon="update" />}
-                    onPress={showModal}
-                />
-                <List.Item
-                    title="Packet size"
-                    description={'Not defined'}
-                    left={(props) => <List.Icon {...props} icon="package-variant-closed" />}
-                />
+                    <Divider />
 
-                <Divider />
+                    <List.Subheader>Speedometer</List.Subheader>
+                    <List.Item
+                        title="Variant style"
+                        description={'Not defined'}
+                        left={(props) => <List.Icon {...props} icon="speedometer" />}
+                    />
 
-                <List.Subheader>Speedometer</List.Subheader>
-                <List.Item
-                    title="Variant style"
-                    description={'Not defined'}
-                    left={(props) => <List.Icon {...props} icon="speedometer" />}
-                />
+                    <Divider />
 
-                <Divider />
+                    <List.Subheader>Maps</List.Subheader>
+                    <List.Item
+                        title="Toggle GPS"
+                        left={(props) => <List.Icon {...props} icon="map-outline" />}
+                        right={(props) => (
+                            <Checkbox status={isSwitchOn ? 'checked' : 'unchecked'} />
+                        )}
+                        onPress={onToggleSwitch}
+                    />
 
-                <List.Subheader>Maps</List.Subheader>
-                <List.Item
-                    title="Toggle GPS"
-                    left={(props) => <List.Icon {...props} icon="map-outline" />}
-                    right={(props) => <Checkbox status={isSwitchOn ? 'checked' : 'unchecked'} />}
-                    onPress={onToggleSwitch}
-                />
+                    <Divider />
 
-                <Divider />
-
-                <List.Subheader>Theme</List.Subheader>
-                <List.Item
-                    title="Dark mode"
-                    left={(props) => <List.Icon {...props} icon="theme-light-dark" />}
-                    right={(props) => <Checkbox status={isSwitchOn ? 'checked' : 'unchecked'} />}
-                    onPress={onToggleSwitch}
-                />
-            </List.Section>
-        </ScrollView>
+                    <List.Subheader>Theme</List.Subheader>
+                    <List.Item
+                        title="Dark mode"
+                        left={(props) => <List.Icon {...props} icon="theme-light-dark" />}
+                        right={(props) => (
+                            <Checkbox status={isSwitchOn ? 'checked' : 'unchecked'} />
+                        )}
+                        onPress={onToggleSwitch}
+                    />
+                </List.Section>
+            </ScrollView>
+        </>
     );
 }
