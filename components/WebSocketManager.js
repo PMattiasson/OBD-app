cb048cf25f8a95ac36fbd32b7215cd70fe199ab6
PMattiasson <pmattiasson99@gmail.com>
2022-12-17 03:16:02 +0100

    useInterval hook to send data from context

--- components/WebSocketManager.js
+++ components/WebSocketManager.js
@@ -1,42 +1,45 @@
-import { useEffect, useRef, useState } from 'react';
-import { TextInput } from 'react-native-paper';
+import { useEffect, useRef } from 'react';
 import { useSettings } from '../context/SettingsContext';
+import { useData } from '../context/DataContext';
+import useInterval from '../hooks/useInterval';
 
 export default function WebSocketManager() {
     const settings = useSettings();
+    const data = useData();
     let ws = useRef(null);
-    const [data, setData] = useState('');
 
     useEffect(() => {
-        const URL = settings.server.apiURL;
-        ws.current = new WebSocket(URL);
-        ws.current.onopen = () => console.log('ws opened');
-        ws.current.onclose = () => console.log('ws closed');
-
-        const wsCurrent = ws.current;
-
-        return () => {
-            wsCurrent.close();
-        };
-    }, [settings.server.apiURL]);
-
-    useEffect(() => {
-        if (!ws.current || !settings.server.toggleUpload) return;
         try {
-            ws.current.send(data);
+            if (settings.server.toggleUpload) {
+                const URL = settings.server.apiURL;
+                ws.current = new WebSocket(URL);
+                ws.current.onopen = () => console.log('ws opened');
+                ws.current.onclose = () => console.log('ws closed');
+
+                const wsCurrent = ws.current;
+
+                return () => {
+                    wsCurrent.close();
+                };
+            }
         } catch (e) {
             console.log(e);
         }
-    }, [data, settings.server.toggleUpload]);
+    }, [settings.server.apiURL, settings.server.toggleUpload]);
 
-    return (
-        <>
-            <TextInput
-                style={{ height: 50, width: '80%' }}
-                label={'Data'}
-                onChangeText={(text) => setData(text)}
-                mode={'outlined'}
-            />
-        </>
+    useInterval(
+        () => {
+            try {
+                const dataSnippet = {
+                    VehicleSpeed: data.VehicleSpeed.value,
+                    EngineRPM: data.EngineRPM.value,
+                };
+                const message = JSON.stringify(dataSnippet);
+                ws.current.send(message);
+            } catch (e) {
+                console.log('WebSocket error:', e);
+            }
+        },
+        settings.server.toggleUpload ? settings.server.uploadFrequency : null,
     );
 }
